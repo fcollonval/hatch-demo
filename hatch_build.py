aafb719370d6ae9e457bbbc826d62b98937606a1
Frédéric Collonval <fcollonval@users.noreply.github.com>
2024-04-17 18:45:25 +0200

    Add some docs

--- hatch_build.py
+++ hatch_build.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from shutil import copyfile, copytree, rmtree, which
 from subprocess import run, CalledProcessError
 from tempfile import TemporaryDirectory
@@ -29,6 +30,8 @@ class OpenAPIMetadataHook(MetadataHookInterface):
         self.path = "src/hatch_demo/spec/openapi.yaml"
 
     def update(self, metadata: dict) -> None:
+        """This updates the metadata mapping of the project table in-place."""
+
         path = os.path.normpath(os.path.join(self.root, self.path))
         if not os.path.isfile(path):
             raise OSError(f"file does not exist: {self.path}")
@@ -68,7 +71,16 @@ class JupyterBuildHook(BuildHookInterface):
         self.path = "src/hatch_demo/spec/openapi.yaml"
         self.target = "src/hatch_demo/openapi_client"
 
+    def clean(self, versions: list[str]) -> None:
+        """This occurs before the build process if the -c/--clean flag
+        was passed to the build command, or when invoking the clean command."""
+        ...
+
     def initialize(self, version: str, build_data: dict[str, t.Any]) -> None:
+        """This occurs immediately before each build.
+
+        Any modifications to the build data will be seen by the build target."""
+
         cmd = which("docker")
         if cmd is None:
             raise ImportError(
@@ -109,8 +121,18 @@ class JupyterBuildHook(BuildHookInterface):
 
             copytree(os.path.join(tmp_dirname, "client", "openapi_client"), self.target)
 
+        # We can do special actions when installing the package in editable mode
+        if version == "editable":
+            readme = Path(self.root).joinpath(self.target, "README.md")
+            readme.parent.mkdir(parents=True, exist_ok=True)
+            readme.write_text(
+                """This README is only generated when installing the package in editable mode."""
+            )
+
     def finalize(
         self, version: str, build_data: dict[str, t.Any], artifact_path: str
     ) -> None:
-        # Available hook
+        """This occurs immediately after each build and will not run if the --hooks-only flag was passed to the build command.
+
+        The build data will reflect any modifications done by the target during the build."""
         ...
